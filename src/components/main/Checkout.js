bba9f0fb72d815ed540004299575d7923ead351b
Lucas Torres <lucas772003@hotmail.com>
2023-08-22 22:20:53 -0300

    Price error fixed

--- src/components/main/Checkout.js
+++ src/components/main/Checkout.js
@@ -1,54 +1,61 @@
-import React, { useContext, useState } from 'react'
-import { CartContext } from '../context/CartContext'
-import {useForm} from "react-hook-form"
-import {collection, addDoc} from "firebase/firestore"
-import { db } from '../../firebase/config'
- 
+import React, { useContext, useState } from 'react';
+import { CartContext } from '../context/CartContext';
+import { useForm } from "react-hook-form";
+import { collection, addDoc } from "firebase/firestore";
+import { db } from '../../firebase/config';
+
 const Checkout = () => {
+    const [pedidoId, setPedidoId] = useState();
+    const { carrito, precioTotal, vaciarCarrito } = useContext(CartContext);
+    const { register, handleSubmit } = useForm();
 
-    const [pedidoId, setPedidoId] = useState()
-
-    const {carrito, precioTotal, vaciarCarrito} = useContext(CartContext)
-    
-    const {register, handleSubmit} = useForm()
- 
     const comprar = (data) => {
         const pedido = {
             cliente: data,
             productos: carrito,
             total: precioTotal()
-        }
-        const pedidosRef = collection(db,"Pedidos")
+        };
+        
+        const pedidosRef = collection(db, "Pedidos");
 
         addDoc(pedidosRef, pedido)
             .then((doc) => {
-                setPedidoId(doc.id)
-                vaciarCarrito()
-            })
-    }
+                setPedidoId(doc.id);
+            });
+    };
 
-    if(pedidoId){
-        return  (
+    const handleCerrarMensaje = () => {
+        setPedidoId(null);
+        vaciarCarrito();
+    };
+
+    if (pedidoId) {
+        return (
             <div className='container'>
                 <h1>Muchas Gracias por tu compra</h1>
                 <p>Tu numero de pedido es: {pedidoId}</p>
-                <p>Para proseguir envia {precioTotal()} al CVU 0000003100037833799807 con los ultimos 4 digitos del ID de tu compra</p>
+                <p>Para proseguir envia ${precioTotal()} al CVU 0000003100037833799807 con los ultimos 4 digitos del ID de tu compra</p>
                 <p>Se acepta transferencia, debito y credito</p>
+                <button onClick={handleCerrarMensaje} className='Cerrar'>Cerrar</button>
             </div>
-        )
+        );
     }
 
-  return (
-    <div className='Container'>
-        <h1 className='title'>Checkout</h1>
-        <form className='formulario' onSubmit={handleSubmit(comprar)}>
-            <input type='text' placeholder='Ingresar tu nombre' {...register("nombre")}/>
-            <input type='email' placeholder='Ingresar tu e-mail' {...register("email")}/>
-            <input type='phone' placeholder='Ingresar tu telefono' {...register("telefono")}/>
-            <button className='enviar' type='submit'>Comprar</button>
-        </form>
-    </div>
-  )
+    return (
+        <div className='Container'>
+            <h1 className='title'>Checkout</h1>
+            <form className='formulario' onSubmit={handleSubmit(comprar)}>
+                <input type='text' placeholder='Ingresar tu nombre' {...register("nombre")} />
+                <input type='email' placeholder='Ingresar tu e-mail' {...register("email")} />
+                <input type='phone' placeholder='Ingresar tu telefono' {...register("telefono")} />
+                <input type='text' placeholder='Direccion' {...register("direccion")} />
+                <input type='text' placeholder='Localidad' {...register("localidad")} />
+                <input type='text' placeholder='Codigo Postal' {...register("CodigoPostal")} />
+
+                <button className='enviar' type='submit'>Comprar</button>
+            </form>
+        </div>
+    );
 }
 
-export default Checkout
+export default Checkout;
